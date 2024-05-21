f24a526e98759d0770ada45d1b37b391e1fe7d77
Kirill.Gorlov <gorlovspace@gmail.com>
2024-05-21 18:09:00 +0300

    fixed bugs

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from src.widget import mask_bank_data
 
-print(mask_bank_data(1234567891111111))
+print(mask_bank_data("visa 1234567891111111"))
--- src/widget.py
+++ src/widget.py
@@ -4,7 +4,7 @@ from .masks import get_mask_numbers
 def mask_bank_data(bank_data: str) -> str:
     data_parts = bank_data.split()
 
-    data_parts[-1] = get_masked_nums(data_parts[-1])
+    data_parts[-1] = get_mask_numbers(data_parts[-1])
 
     return " ".join(data_parts)
 
