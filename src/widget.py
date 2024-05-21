cede097a6d2809f8981d343703dc469ae61d2c92
Kirill.Gorlov <gorlovspace@gmail.com>
2024-05-21 21:50:14 +0300

    Nichego ne izmenil

--- src/widget.py
+++ src/widget.py
@@ -2,15 +2,15 @@ from .masks import get_mask_numbers
 
 
 def get_clear_data(data_time: str) -> str:
-    '''Функция, которая получает дату со временем и возвращает дату'''
-    new_data_list = (data_time.split("T"))
+    """Функция, которая получает дату со временем и возвращает дату"""
+    new_data_list = data_time.split("T")
     data = new_data_list[0].split("-")
 
     return f"{data[-1]}-{data[-2]}-{data[-3]}"
 
 
 def mask_bank_data(bank_data: str) -> str:
-    '''Функция, которая отделяет название карты и счета от номера'''
+    """Функция, которая отделяет название карты и счета от номера"""
 
     data_parts = bank_data.split()
     data_parts[-1] = get_mask_numbers(data_parts[-1])
