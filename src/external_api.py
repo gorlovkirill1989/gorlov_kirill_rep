4806dc1a9b157ddd13a096ec1a20bd66aa9a3285
Kirill.Gorlov <gorlovspace@gmail.com>
2024-06-27 13:18:24 +0300

    Поправлен gitignor. В функцию get_transactions_data добавлена возможность считывания xlsx и csv, дополнен README

--- src/external_api.py
+++ src/external_api.py
@@ -6,9 +6,6 @@ from dotenv import load_dotenv
 # Загрузка переменных из .env-файла
 load_dotenv()
 
-# Отправка GET-запроса к API
-# response = requests.get('https://api.github.com/user', headers=headers)
-
 
 def convert(amount: float, currency_from: str, currency_to: str = "RUB") -> float:
     """метод конвертации из одной валюты в другую с помощью apilayer"""
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+import pandas as pd
+
 from config import ROOT_DIR
 from src.external_api import convert
 from src.logger import create_logger
@@ -9,18 +11,42 @@ logger = create_logger("utils")
 
 
 def get_transactions_data(file_path: str) -> list[dict]:
-    """функция, которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    """функция, которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых
+    транзакциях"""
+
+    # записываем путь до до файла и забираем расширение указывая индекс [-1]
+    file_extension = os.path.splitext(file_path)[-1]
 
     try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            json_data_transactions = json.load(file)
+        if file_extension == ".csv":
+            file_data = pd.read_csv(file_path, delimiter=";", encoding="utf8")
 
-            # Проверяем, является ли содержимое списком
-            if not isinstance(json_data_transactions, list):
-                return []
-                logger.info("содержимое файла JSON не является списком")
-            logger.info("содержимое файла JSON успешно передано")
-            return json_data_transactions
+            # преобразуем в список словарей
+            data_frame_file_data = file_data.to_dict("records")
+
+            logger.info("содержимое файла csv успешно передано")
+            return data_frame_file_data
+
+        elif file_extension == ".xlsx":
+            file_data = pd.read_excel(file_path)
+
+            # преобразуем в список словарей
+            data_frame_file_data = file_data.to_dict("records")
+
+            logger.info("содержимое файла xlsx успешно передано")
+            return data_frame_file_data
+
+        else:
+
+            with open(file_path, "r", encoding="utf-8") as file:
+                json_data_transactions = json.load(file)
+
+                # Проверяем, является ли содержимое списком
+                if not isinstance(json_data_transactions, list):
+                    return []
+                    logger.info("содержимое файла JSON не является списком")
+                logger.info("содержимое файла JSON успешно передано")
+                return json_data_transactions
 
     except json.JSONDecodeError:
         # Если файл не JSON
@@ -46,8 +72,8 @@ def get_transaction_amount(transaction: dict) -> float:
 
 
 if __name__ == "__main__":
-
-    path_to_file = os.path.join(ROOT_DIR, "data", "operations.json")
+    # path_to_file = os.path.join(ROOT_DIR, "data", "transactions.csv")
+    path_to_file = os.path.join(ROOT_DIR, "data", "transactions_excel.xlsx")
 
     operations = get_transactions_data(path_to_file)
-
+    print(operations)
